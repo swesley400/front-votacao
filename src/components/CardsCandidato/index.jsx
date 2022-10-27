65f47c7f11fdcab702b801b368af125526f2bfa1
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-10-26 22:46:27 -0300

    front version 1.0.0

---
+++ src/components/CardsCandidato/index.jsx
@@ -0,0 +1,56 @@
+import './style.scss'
+
+export default function CardCandidatos(props){
+
+    const host = "https://app-votacao-wes.herokuapp.com/"
+   
+
+    function vote(doc, name){
+        const body = {
+                "id":doc, 
+                "candidato": name
+            }
+        console.log(body)
+        
+
+        fetch(`${host}votar`, {
+            
+            // Adding method type
+            method: "POST",
+            
+            // Adding body or contents to send
+            body: JSON.stringify(body),
+            
+            // Adding headers to the request
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        
+        // Converting to JSON
+        .then(response => response.json())
+        
+        // Displaying results to console
+    }
+
+
+    return(
+    <div className='cardCandidatos'>
+        <div className='img-candidato'>
+            <img src={props.urlImg} alt="imgCandidato" />
+        </div>
+        <div>{props.nome}</div>
+        <div className='partido-numero'>
+            <div>{props.partido}</div>
+            <div>{props.nume}</div>
+        </div>
+        <div className='votos'>{props.quandidade_de_votos}</div>
+        <div className='barra-progresso-out'>
+            <div className='barra-progresso-in'>
+                <span className='texto-barra'>100%</span>
+            </div>
+        </div>
+        <button className='votar' onClick={()=>{ vote(props.docs, props.nome)}}>Votar</button>
+    </div>
+    )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import CardCandidatos from '../../components/CardsCandidato';
+import './style.scss'
+
+function App() {
+  const [dadosApi, setDadosApi] = useState([])
+
+  const host = "https://app-votacao-wes.herokuapp.com/"
+
+  useEffect(() => {
+    
+      
+    
+    fetch(host)
+    .then(response => response.json())
+    .then( data => setDadosApi(data))
+    .catch(err => console.log("Houve um erro", err))
+    
+    
+
+  
+  }, []);
+
+ 
+ 
+  return (
+    <div className="App">
+      <h1>Versus</h1>
+      <div className='container-Candidatos'>
+        {dadosApi.map((item) =>  <CardCandidatos 
+          key={item.doc}
+          urlImg={item.urlImg}
+          nome={item.nome} 
+          partido={item.partido} 
+          nume={item.numero} 
+          quandidade_de_votos={item.quantidade_de_votos}
+          docs={item.doc}
+          porcetagem= "50"
+          >
+          
+        </CardCandidatos>)}
+      
+      </div>
+    </div>
+  )
+}
+
+export default App
