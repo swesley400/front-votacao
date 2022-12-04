c1fb2ecbb6d16c1ff88f4e0ef605aa1ab5610898
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2022-12-04 01:36:21 -0300

    alteracoes finais

--- src/components/CardsCandidato/index.jsx
+++ src/components/CardsCandidato/index.jsx
@@ -2,35 +2,22 @@ import './style.scss'
 
 export default function CardCandidatos(props){
 
-    const host = "https://app-votacao-wes.herokuapp.com/"
+    const host = "http://localhost:3000/"
    
-
     function vote(doc, name){
         const body = {
                 "id":doc, 
                 "candidato": name
             }
-        console.log(body)
-        
 
         fetch(`${host}votar`, {
-            
-            // Adding method type
             method: "POST",
-            
-            // Adding body or contents to send
             body: JSON.stringify(body),
-            
-            // Adding headers to the request
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
             }
         })
-        
-        // Converting to JSON
         .then(response => response.json())
-        
-        // Displaying results to console
     }
 
 
@@ -47,8 +34,8 @@ export default function CardCandidatos(props){
         </div>
         <div className='votos'>{props.quandidade_de_votos}</div>
         <div className='barra-progresso-out'>
-            <div className='barra-progresso-in'>
-                <span className='texto-barra'>100%</span>
+            <div className='barra-progresso-in' style={{width: `${props.porcetagem}%`}}>
+                <span className='texto-barra'>{props.porcetagem}%</span>
             </div>
         </div>
         <button className='votar' onClick={()=>{ vote(props.docs, props.nome)}}>Votar</button>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,27 +6,37 @@ import Footer from '../../components/Footer';
 import './style.scss'
 
 
+
 function App() {
   const [dadosApi, setDadosApi] = useState([])
+  const [votosGerais, setVotosGerais] = useState([])
+  const host = "http://localhost:3000/"
 
-  const host = "https://app-votacao-wes.herokuapp.com/"
+
+  function calculaPorcetagem(quantidadeDeVotosPorCanditado, votosGerais){
+    let porcetagem = Number(quantidadeDeVotosPorCanditado)*100/ votosGerais
+    return porcetagem
+  }
+
+ 
+  useEffect(() => {
+    fetch(`${host}votosgerais`)
+    .then(response => response.json())
+    .then( data => setVotosGerais(data.votosGerais))
+    .catch(err => console.log("Houve um erro", err))
+    
+  }, [votosGerais]);
+
+
 
   useEffect(() => {
-    
-      
-    
     fetch(host)
     .then(response => response.json())
     .then( data => setDadosApi(data))
     .catch(err => console.log("Houve um erro", err))
     
-    
-
+  }, [dadosApi, votosGerais]);
   
-  }, [dadosApi]);
-
- 
- 
   return (
     <div className="App">
       <h1>Versus</h1>
@@ -39,7 +49,7 @@ function App() {
           nume={item.numero} 
           quandidade_de_votos={item.quantidade_de_votos}
           docs={item.doc}
-          porcetagem= "50"
+          porcetagem={ Math.round(calculaPorcetagem(item.quantidade_de_votos, votosGerais))}
           >
           
         </CardCandidatos>)}
